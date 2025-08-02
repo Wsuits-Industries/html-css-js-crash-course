8dd75da0ab4db58f910bb9ab35e1061a50843adc
b0dmas <wsuits6@gmail.com>
2025-08-02 00:55:48 +0000

    EP8: Added interactive contact form with JavaScript validation

---
+++ script.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("contact-form")
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    alert("Thanks soo Much for Submiting the form, Please suibscribe!!!")
+})
